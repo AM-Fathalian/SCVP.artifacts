fef053490e4401e406f5f39023cd6e64d99486de
AM_Fathalian <fathalian1379@gmail.com>
2026-03-14 17:09:09 +0100

    exercise5 donw, lesson learned: every write() and read() calls for FIFOs in a function will read the another variable. + we can declare processes in .h file and define them in a .cpp file

--- exercise5/kpn.cpp
+++ exercise5/kpn.cpp
@@ -2,5 +2,43 @@
 #include <systemc.h>
 
 #include "kpn.h"
+using namespace std;
 
-// ADD THINGS HERE
+void kpn::split(){
+    while (true){ // with while always do return or break somewhere. (what is the difference??)
+        // wait(5, SC_NS);
+        e = b.read();
+        a.write(e);
+        d.write(e);
+        
+        ++cnt;
+        cout << " @ " << sc_time_stamp() << " e = " << e << endl;
+        if(cnt >= 10){
+            // wait(SC_ZERO_TIME);
+            sc_stop();
+            break;
+        }
+    }
+}
+
+
+void kpn::add(){
+
+    while(true){
+        // wait(sc_time(1,SC_NS));
+        b.write(a.read() + c.read());
+    }
+}
+
+
+void kpn::delay(){
+    while(true){
+        // wait(sc_time(10,SC_NS));
+        c.write(d.read());
+    }
+}
+    // virtual void process() = 0;
+
+
+
+
--- exercise5/kpn.h
+++ exercise5/kpn.h
@@ -3,15 +3,50 @@
 
 #include <systemc.h>
 
+
+
+// template<class T>
+// SC_MODULE(operater){
+
+//     private:
+//     sc_fifo
+//     virtual void split() = 0;
+//     virtual void add() = 0;
+//     virtual void delay() = 0;
+//     public:
+//     SC_CTOR(operator): {
+
+//     }
+// };
+
+
 SC_MODULE(kpn)
 {
 private:
-    // ADD THINGS HERE
+
+    sc_fifo<unsigned int> a, b, c, d;
+    unsigned int e;
+     void split();
+     void add();
+     void delay();
+    //  void process();
+    unsigned int cnt = 0;
 
 public:
-    SC_CTOR(kpn) // : ADD THINGS HERE
+    SC_CTOR(kpn) : a(10), b(10), c(10), d(10) // declare FIFOs depth here 
     {
-        // ADD THINGS HERE
+        b.write(1);
+        c.write(0);
+        SC_THREAD(split);
+        SC_THREAD(add);
+        SC_THREAD(delay);
+        // SC_THREAD(process);
+        sensitive << a.data_read_event() << a.data_written_event()
+                  << b.data_read_event() << b.data_written_event()
+                  << b.data_read_event() << b.data_written_event()
+                  << c.data_read_event() << c.data_written_event()
+                  << d.data_read_event() << d.data_written_event();
+                    
     }
 };
 
